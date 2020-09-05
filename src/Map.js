4be4c023574ccda7bf59ed73a31dce3429a7d43e
ZhuoranS <zhuoran.su.ufo@gmail.com>
2020-09-05 11:45:38 -0500

    Update Map.js

--- src/Map.js
+++ src/Map.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { GoogleMap, LoadScript, Marker, Rectangle } from '@react-google-maps/api';
-import { Button } from 'react-bootstrap';
+import { Modal, Button } from 'antd';
+
 
   const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
@@ -10,12 +11,12 @@ import { Button } from 'react-bootstrap';
     display: 'flex',  
     justifyContent:'center', 
     alignItems:'center'
-  };
+  }
    
   const campusCenter = {
     lat: 42.051985,
     lng: -87.675863
-  };
+  }
 
   const areaBounds = { 
     south: 42.035,
@@ -30,26 +31,81 @@ import { Button } from 'react-bootstrap';
       fillOpacity: 0.1
   }
   
+  const gridSize = 7
+  const zoomRate = 14
+
+  function handleClick(bound){
+
+  }
+  
+  class Grid extends React.Component {
+    constructor(props){
+        super(props);
+        this.bound = props.bound;
+    }
+
+    state = { visible: false };
+
+    showModal = () => {
+        this.setState({
+        visible: true,
+        });
+    };
+
+    handleOk = e => {
+        console.log(e);
+        this.setState({
+        visible: false,
+        });
+    };
+
+    handleCancel = e => {
+        console.log(e);
+        this.setState({
+        visible: false,
+        });
+    };
+
+    render() {
+        return (
+        <>
+            <Rectangle bounds={this.bound} 
+            options={rectOptions} 
+            onClick={this.showModal}/>
+            <Modal
+            title="Basic Modal"
+            visible={this.state.visible}
+            onOk={this.handleOk}
+            onCancel={this.handleCancel}
+            >
+            <p>{this.bound.north}</p>
+            <p>{this.bound.south}</p>
+            <p>{this.bound.east}</p>
+            <p>{this.bound.west}</p>
+            </Modal>
+        </>
+        );
+    }
+}
+
   function AreaRow(props){
       function option(e){  
         return {
-        north: props.south +  areaSize.height,
+        north: props.south + areaSize.height,
         south: props.south,
         east: props.east - e * areaSize.width,
         west: props.east - (e + 1) * areaSize.width
         }
       }
-      return([...Array(7).keys()].map(
+      return([...Array(gridSize).keys()].map(
           e =>
-          <Rectangle 
-          bounds={option(e)}
-          options={rectOptions} />
+          <Grid bound={option(e)} key={e}/>
           ))
   }
 
   function AreaMap(props){
-      return([...Array(7).keys()].map(
-          e => <AreaRow south={props.south + e * areaSize.height} east = {props.east}/>
+      return([...Array(gridSize).keys()].map(
+          e => <AreaRow south={props.south + e * areaSize.height} east = {props.east} key={e}/>
           ))
   }
 
@@ -63,7 +119,7 @@ import { Button } from 'react-bootstrap';
           mapContainerStyle={containerStyle}
           defaultCenter={campusCenter}
           center={campusCenter}
-          zoom = {14}
+          zoom = {zoomRate}
           
         >
           <Marker position={campusCenter}/>
